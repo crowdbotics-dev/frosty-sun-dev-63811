fddd6f1a9e88f2b2177221fce8f91813ceb67285
Crowdbotics <engineering@crowdbotics.com>
2023-02-15 11:34:17 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/frosty_sun_dev_63811/MainActivity.java
@@ -0,0 +1,15 @@
+package com.frosty_sun_dev_63811;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "frosty_sun_dev_63811";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frosty-sun-dev-63811",
+  url: "https://frosty-sun-dev-63811.botics.co",
+  api: "https://frosty-sun-dev-63811.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
